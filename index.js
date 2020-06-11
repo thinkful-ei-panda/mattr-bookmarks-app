1051caed22e97ae6023608c00bd0730a090e89bd
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-11 08:25:52 -0700

    boilerplate code for app
    ready to modify

--- index.js
+++ index.js
@@ -1,87 +1,21 @@
-const store = {
-    bookmarks: [
-      {
-        id: 'x56w',
-        title: 'Title 1',
-        rating: 3,
-        url: 'http://www.title1.com',
-        description: 'lorem ipsum dolor sit',
-        expanded: false
-      },
-      {
-        id: '6ffw',
-        title: 'Title 2',
-        rating: 5,
-        url: 'http://www.title2.com',
-        description: 'dolorum tempore deserunt',
-        expanded: false
-      } 
-      ...
-    ],
-    adding: false,
-    error: null,
-    filter: 0
-  };
+import $ from 'jquery';
 
-  function generateHomeScreen(){
+import 'normalize.css';
+import './index.css';
 
-  }
+import api from './api';
+import store from './store';
+import bookmarksList from './bookmarks-list';
 
-  function generateAddScreen(){
+const main = function () {
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      bookmarksList.render();
+    });
 
-  }
+  bookmarksList.bindEventListeners();
+  bookmarksList.render();
+};
 
-  function generateDeleteScreen(){
-
-  }
-
-  function generateOpenItem(){
-
-  }
-
-  
-
-  function renderHomeScreen(){
-
-  }
-
-  function renderAddScreen(){
-
-  }
-
-  function renderDeleteScreen(){
-
-  }
-
-  function renderOpenItem(){
-    
-  }
-
-  function getBookmarks(){
-    
-  }
-
-  function addBookmarks(){
-
-  }
-
-  function deleteBookmarks(){
-
-  }
-
-  function getBookmarksId(){
-
-  }
-
-
-  function handleSubmit(){
-
-  }
-
-  function handleDelete(){
-
-  }
-
-  function handleOpenItem(){
-
-  }
+$(main);
