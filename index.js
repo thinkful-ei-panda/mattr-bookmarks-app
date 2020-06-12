743884b2abdb355b3c90ed0f5913a12aa90809d8
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-11 19:04:59 -0700

    Started over from scratch
    Added generate and render hom screen and add screen. Making css changes as I go along.

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
-// import 'normalize.css';
-import './index.css';
-
-import api from './api.js';
-import store from './store.js';
-import bookmarksList from './bookmarks-list.js';
-
-const main = function () {
-  api.getItems()
-    .then((items) => {
-      items.forEach((item) => store.addItem(item));
-      bookmarksList.render();
-    });
-
-  bookmarksList.bindEventListeners();
-  bookmarksList.render();
+const generateHomeScreen = function () {
+  $('main').html(`
+  <form id="add-filter">
+    <ul class="two-buttons">
+    <button class="btn" class="add-btn">New</button>
+    <button class="btn">Filter</button>
+    </ul>
+    </form>
+    <section>
+    <ul class="bookmarks-list"> 
+        <li>
+            <h2>Name of Bookmark</h2>
+            <h2> </h2>
+        </li>
+    </ul>
+    </section>`);
 };
 
-$(main);
+function renderHomeScreen(){
+  generateHomeScreen();
+}
+$(renderHomeScreen);
 
+const generateAddScreen = function () {
+  $('main').html(`
+  <form class="add-bookmark" id="add-bookmark-api">
+  <input class="bookmark-input" type="text" value="name" placeholder="hi" required />
+  
+  <textarea class="bookmark-input" type="text" value="desc" placeholder="description" required />
+  <input class="bookmark-input" type="text" value="url" placeholder="hi" required />
+  <div class="two-buttons">
+  <button class="btn" type="submit" id="add-bookmark">Save</button>
+  <button class="btn" type="click" id="cancel">Cancel</button>
+  </div>
+  </form>`);
+};
+  
+function renderAddScreen(){
+  generateAddScreen();
+}
 
+// $(renderAddScreen);
+
+function handleAddButton() {
+  $('main').on("click", "#add-filter", event =>{
+    event.preventDefault();
+    console.log(`handle Add Button is running`)
+    return renderAddScreen();
+  })
+}
+
+$(handleAddButton)
+
+// function handlerReset(){
+//     $('main').on('click', '#add-btn',  event => {
+//       event.preventDefault();
+//       console.log(`handler reset working`);
+//       return $(renderAddScreen);
+//     });
+//   }
