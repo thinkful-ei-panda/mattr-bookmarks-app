11ed96329a2e3a97f1d9e89397c766c5b4f34224
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-13 12:14:06 -0700

    BUG FIXED in index.js
    populates upon first rendering home screen

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ import bookmarks from './bookmarks.js';
 import store from './store.js';
 import api from './api.js';
 
-const main = function () {
-  console.log(`main is running`)
+// const main = function () {
+//   console.log(`main is running`)
   
 
+//   api.getBookmarks()
+//     .then((bookmarks) => {
+//       bookmarks.forEach((bookmark) => store.addBookmark(bookmark));
+//     });
+//   bookmarks.renderHomeScreen();
+// };
+
+// $(main);
+
+const main = function () {
+  console.log(`main is running`)
+   
   api.getBookmarks()
     .then((bookmarks) => {
       bookmarks.forEach((bookmark) => store.addBookmark(bookmark));
-    });
-  bookmarks.renderHomeScreen();
+    }).then(() => bookmarks.renderHomeScreen());
 };
-
+ 
 $(main);
-
